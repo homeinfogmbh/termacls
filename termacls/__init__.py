31be59baec34ef52c32f975bb306c6a7d96ee9f2
Richard Neumann <r.neumann@homeinfo.de>
2020-10-23 17:56:36 +0200

    Fix.

--- termacls/__init__.py
+++ termacls/__init__.py
@@ -5,7 +5,9 @@ Manages access to deployments and systems for administration and setup.
 from termacls.functions import can_administer_deployment
 from termacls.functions import can_administer_system
 from termacls.functions import can_deploy
+from termacls.functions import get_deployment_admin_condition
 from termacls.functions import get_administerable_deployments
+from termacls.functions import get_system_admin_condition
 from termacls.functions import get_administerable_systems
 
 
@@ -13,6 +15,8 @@ __all__ = [
     'can_administer_deployment',
     'can_administer_system',
     'can_deploy',
+    'get_deployment_admin_condition',
     'get_administerable_deployments',
+    'get_system_admin_condition',
     'get_administerable_systems'
 ]
