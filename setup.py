dda0cc23eb65dc65cb3ba65add26cccd22c4bd41
Richard Neumann <r.neumann@homeinfo.de>
2022-03-30 13:24:30 +0200

    Migrate to setuptools and add dependencies

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 #! /usr/bin/env python3
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='termacls',
+    use_scm_version={
+        "local_scheme": "node-and-timestamp"
+    },
+    setup_requires=['setuptools_scm'],
+    install_requires=[
+        'his',
+        'hwdb',
+        'peewee',
+        'peeweeplus'
+    ],
     author='HOMEINFO - Digitale Informationssysteme GmbH',
     author_email='info@homeinfo.de',
     maintainer='Richard Neumann',
     maintainer_email='r.neumann@homeinfo.de',
     packages=['termacls'],
-    description=("HOMEINFO's terminal ACL libary."))
+    description="HOMEINFO's terminal ACL libary."
+)
